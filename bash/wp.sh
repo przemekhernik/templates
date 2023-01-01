7f8a40d8defbbf6968767c51362285bd5a533547
Przemysław Hernik <przemek.hernik@hotmail.com>
2023-01-01 14:04:43 +0100

    update templates

--- bash/wp.sh
+++ bash/wp.sh
@@ -41,8 +41,8 @@ case $1 in
     mysql --user=$DB_USER --password=$DB_PASS --host=$DB_HOST --port=$DB_PORT --execute="CREATE DATABASE $DB_NAME;"
 
     wp core download
-    wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST
-    wp core install --url=$PROJECT_DOMAIN --title=$PROJECT_NAME --admin_user=$ADMIN_LOGIN --admin_password=$ADMIN_PASS --admin_email="$ADMIN_EMAIL" --skip-email
+    wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --dbcharset=utf8mb4 --dbcollate=utf8mb4_general_ci
+    wp core install --url=https://$PROJECT_DOMAIN --title=$PROJECT_NAME --admin_user=$ADMIN_LOGIN --admin_password=$ADMIN_PASS --admin_email="$ADMIN_EMAIL" --skip-email
     wp plugin delete hello akismet
     wp theme delete twentytwentyone twentytwentytwo
     wp plugin install wordpress-seo
@@ -61,11 +61,23 @@ case $1 in
     curl https://raw.githubusercontent.com/przemekhernik/templates/main/htaccess/.htaccess.wp -o .htaccess
     curl https://raw.githubusercontent.com/przemekhernik/templates/main/htaccess/.htpasswd.wpt -o .htpasswd
 
-    open "http://$PROJECT_DOMAIN"
+    open "https://$PROJECT_DOMAIN"
     ;;
 
   "db:export")
     mysqldump --user=root --password=root --host=127.0.0.1 acme | gzip > db.sql.gz
+    ;;
+
+  "db:export:staging")
+    wp search-replace acme.test acme.staging --all-tables
+    mysqldump --user=root --password=root --host=127.0.0.1 acme | gzip > db.sql.gz
+    wp search-replace acme.staging acme.test --all-tables
+    ;;
+
+  "db:export:prod")
+    wp search-replace acme.test acme.prod --all-tables
+    mysqldump --user=root --password=root --host=127.0.0.1 acme | gzip > db.sql.gz
+    wp search-replace acme.prod acme.test --all-tables
     ;;
 
   "db:import")
@@ -74,7 +86,25 @@ case $1 in
     rm db.sql
     ;;
 
+  "db:import:staging")
+    gzip -d db.sql.gz
+    mysql --user=root --password=root --host=127.0.0.1 acme < db.sql
+    rm db.sql
+    wp search-replace acme.staging acme.test --all-tables
+    ;;
+
+  "db:import:prod")
+    gzip -d db.sql.gz
+    mysql --user=root --password=root --host=127.0.0.1 acme < db.sql
+    rm db.sql
+    wp search-replace acme.prod acme.test --all-tables
+    ;;
+
   *)
     echo "ERR: No task found."
     ;;
 esac
+
+
+
+      
