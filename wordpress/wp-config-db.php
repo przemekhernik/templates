fc81650b7ce5eb012a64d44e413fc95dfb6b0aba
Przemek Hernik <przemek.hernik@hotmail.com>
2025-02-04 11:48:15 +0100

    Update wp-config-db.php

--- wordpress/wp-config-db.php
+++ wordpress/wp-config-db.php
@@ -13,7 +13,7 @@ define( 'DB_PASSWORD', 'root' );
 define( 'DB_HOST', '127.0.0.1:3306' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
